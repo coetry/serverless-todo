9488de5aad498e3e8dfd0bfdb6ad3cd1f6834a9f
Allen Hai <allenhai03@gmail.com>
2019-04-06 13:11:11 -0400

    change header color to red

--- components/header.js
+++ components/header.js
@@ -2,6 +2,12 @@ function Header() {
   return (
     <header>
       <h1>Next.js Example on Now 2.0</h1>
+      <style jsx>{`
+        h1 {
+          color: red;
+          text-decoration: underline;
+        }
+      `}</style>
     </header>
   );
 }
